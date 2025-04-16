1d3864b2b2bae762714482d5b761bfd28319da8b
igorhgds <igorhenrique.gomes@hotmail.com>
2025-04-16 13:04:01 -0300

    add application.yaml

--- src/main/java/igor/henrique/desafio_senai_restaurante/entities/User.java
+++ src/main/java/igor/henrique/desafio_senai_restaurante/entities/User.java
@@ -1,4 +1,33 @@
 package igor.henrique.desafio_senai_restaurante.entities;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity(name = "users")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id", columnDefinition = "uuid", updatable = false, nullable = false)
+    private long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
 }
