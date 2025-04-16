a9aa5ba2a0dd66927485098a3f466eaf4a1a22e0
igorhgds <igorhenrique.gomes@hotmail.com>
2025-04-16 12:13:11 -0300

    update projeto base

---
+++ src/main/java/igor/henrique/desafio_senai_restaurante/DesafioSenaiRestauranteApplication.java
@@ -0,0 +1,13 @@
+package igor.henrique.desafio_senai_restaurante;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioSenaiRestauranteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioSenaiRestauranteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/igor/henrique/desafio_senai_restaurante/DesafioSenaiRestauranteApplicationTests.java
@@ -0,0 +1,13 @@
+package igor.henrique.desafio_senai_restaurante;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioSenaiRestauranteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
